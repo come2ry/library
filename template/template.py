3702467899f14d5f9f7ea1745674d288017289ef
come2ry <raiki5100@gmail.com>
2022-01-16 19:08:54 +0900

    MODIFY: スッキリ

--- template/template.py
+++ template/template.py
@@ -1,29 +1,15 @@
 # WaveletMatrix
 from typing import *
-from functools import singledispatch, update_wrapper
-
-T = TypeVar('T', int, float)
-D = TypeVar('D', int, float)
 
 
-def singledispatchmethod(func):
-    dispatcher = singledispatch(func)
-
-    def wrapper(*args, **kw):
-        return dispatcher.dispatch(args[1].__class__)(*args, **kw)
-    wrapper.register = dispatcher.register
-    update_wrapper(wrapper, func)
-    return wrapper
-
-
-def chmax(a: T, b: T) -> Tuple[T, bool]:
+def chmax(a: Any, b: Any) -> Tuple[Any, bool]:
     if (a < b):
         a = b  # aをbで更新
         return (a, True)
     return (a, False)
 
 
-def chmin(a: T, b: T) -> Tuple[T, bool]:
+def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
 
     if (a > b):
         a = b  # aをbで更新
